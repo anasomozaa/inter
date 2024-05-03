b4d26f646cdbf4a20604b22107634853b9d30dc0
anasomozaa <52749830+anasomozaa@users.noreply.github.com>
2024-05-03 21:59:51 +0200

    Update ana_silvia.py

--- ana_silvia.py
+++ ana_silvia.py
@@ -109,6 +109,6 @@ grants = visualizechart()
 st.write(grants)
 
 option = st.selectbox('Choose an Activity', grants['activityType'].unique())
-st.bar_chart(grants[['ecContribution']][option])  # Access the multi-level index using a tuple
+st.bar_chart(grants[option])  # Access the multi-level index using a tuple
 
 conn.close()
