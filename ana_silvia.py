e46b3c81837150865fb890b34fe902d8dbe85b30
Silviaprietoo <94482427+Silviaprietoo@users.noreply.github.com>
2024-05-05 21:08:09 +0200

    Update ana_silvia.py

--- ana_silvia.py
+++ ana_silvia.py
@@ -96,7 +96,7 @@ st.download_button(label="Project Coordinators CSV",data=convert_projectcoordina
 import streamlit as st
 
 # Display a graph with the evolution of received grants of the partners in a country according to their activityType.
-st.title(f'Evolution of received grants per partners according to Activity Type - {selected_country}')
+st.title(f'Evolution of received grants per partners according to Activity Type - {acronym_c}')
 
 # Filter data for the selected country
 df_country = df2[df2['Acronym'] == acronym_c]
